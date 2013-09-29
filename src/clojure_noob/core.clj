e15c86a77a32a0db6cc2011ec1c55fc2a4a471c6
DjebbZ <khalid.jebbari@gmail.com>
2013-09-29 19:11:45 +0200

    Create and access a vector of strings

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -5,3 +5,11 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Yo!"))
+
+(def failed-protagonist-names
+  ["Larry Potter"
+   "Doreen the Explorer"
+   "The Incredible Bulk"])
+
+(failed-protagonist-names 2)
+
