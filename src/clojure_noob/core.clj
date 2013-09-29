3acc6e6226629d34dd2e0a1dae6f419e791f1eef
DjebbZ <khalid.jebbari@gmail.com>
2013-09-29 19:15:31 +0200

    Add print statements + comments

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -1,15 +1,21 @@
 (ns clojure-noob.core
   (:gen-class))
 
+; main entry point of program
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Yo!"))
 
+; a vector is like an array in JS
 (def failed-protagonist-names
   ["Larry Potter"
    "Doreen the Explorer"
    "The Incredible Bulk"])
 
+; Accessing a vector means using the vector as a function
 (failed-protagonist-names 2)
 
+; Simple print statements
+(println "Something")
+(println "With \"quotes\" inside")
